9e1fd929c1291e435c1f2d576a97b7ea2a95393a
singhnandnibgs01-lang <singhnandnibgs01@gmail.com>
2026-06-20 22:04:09 +0530

    Create Three number.js

---
+++ Three number.js
@@ -0,0 +1,16 @@
+let a=20;
+let b=40;
+let c=35;
+
+if(a>=b && b>=c)
+{
+  console.log(a + "is largest");
+              }
+else if(b>=a && c>=a)
+{
+  console.log(b + "is largest");
+}
+else{
+  console.log(c + " is largest");
+}
+
